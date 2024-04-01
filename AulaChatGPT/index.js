31a1ab74661f2cf4079ecff0483524e3d3f88873
indigo-mulinari <indigo.saturno@gmail.com>
2024-04-01 10:55:35 -0300

    mudanças 01/04
    Adicionando a ligação com o chat gpt

--- AulaChatGPT/index.js
+++ AulaChatGPT/index.js
@@ -8,10 +8,23 @@ app.use(express.json())
 const OPENAI_API_KEY = process.env.OPENAI_API_KEY
 const openai = new OpenAI(OPENAI_API_KEY)
 
-app.post('/pergunte-ao-chatgpt',(req,res) => {
+app.post('/pergunte-ao-chatgpt',async (req,res) => {
     const { prompt } = req.body
-    console.log(prompt)
-    res.json({seuPrompt: prompt})
+    
+    const model = 'gpt-3.5-turbo'
+    const role = 'user'
+    const max_tokens = 30
+
+    //comunicação com o chat
+    const completion = await openai.chat.completions.create({
+        messages: [{role: role, content: prompt}],
+        model: model,
+        max_tokens: max_tokens
+    });
+
+    res.json({
+        completion: completion.choices[0].message.content
+    })
 })
 const PORT = 4000
 app.listen(PORT, () => console.log(`Em execução na porta ${PORT}`))
